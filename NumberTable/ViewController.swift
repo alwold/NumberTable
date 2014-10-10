3c4587b65344c68e5591ca470e7072c940a6a9c4
Al Wold <alwold@alwold.com>
2014-10-09 22:46:42 -0700

    commit all of the code

--- NumberTable/ViewController.swift
+++ NumberTable/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minNumber: UITextField!
+    @IBOutlet weak var maxNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var min = minNumber.text.toInt()
+        var max = maxNumber.text.toInt()
+        NSLog("minNumber \(min) maxNumber \(max)")
+        if segue.destinationViewController is TableViewController {
+            var tvc: TableViewController = segue.destinationViewController as TableViewController
+            tvc.min = min
+            tvc.max = max
+        } else {
+            NSLog("BIG PROBLEMS")
+        }
+    }
 
 }
 
